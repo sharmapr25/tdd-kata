5ff442e1bb70638b0c3d6606570cecf38d2f1c9f
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-06-23 10:57:43 +0530

    Added a new test for provide sum when given number is one and update java version from 1.7 to 1.8

--- src/main/java/com/kata/StringCalculator.java
+++ src/main/java/com/kata/StringCalculator.java
@@ -1,9 +1,18 @@
 package com.kata;
+import java.util.*;
+import java.util.stream.*;
+import java.util.Arrays;
 
 public class StringCalculator{
     public StringCalculator(){};
 
     public int add(String numbers){
-        return 0;
+        if(numbers.length() == 0) return 0;
+        List<String> numbersList = Arrays.asList(numbers.split(""));
+        int sum = 0;
+        for(String number: numbersList){
+            sum += Integer.valueOf(number);
+        }
+        return sum;
     };
 }
--- src/test/java/com/kata/StringCalculatorTest.java
+++ src/test/java/com/kata/StringCalculatorTest.java
@@ -15,4 +15,11 @@ public class StringCalculatorTest
         int sum = stringCalculator.add("");
         assertEquals(0, sum);
     };
+
+    @Test
+    public void add_shouldReturnOne_WhenGivenInputHasNumberOne(){
+        StringCalculator stringCalculator = new StringCalculator();
+        int sum = stringCalculator.add("1");
+        assertEquals(1, sum);
+    }
 }
